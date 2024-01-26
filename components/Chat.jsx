926ee702f6846f4a8d4fe5c2455bb901a60fad92
Pritam Prasanjeet Behera <99427618+Pritam08pb@users.noreply.github.com>
2024-01-26 16:03:44 +0530

    Update Chat.jsx
    fixed some issues .

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -2,12 +2,13 @@ import styles from "../styles/chat.module.css";
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-
 let socket;
 
 const Chat = () => {
   const [message, setMessage] = useState("");
   const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+  const [currentuser,setcurrent]= useState("");
   const [allMessages, setAllMessages] = useState([]);
 
   useEffect(() => {
@@ -16,27 +17,34 @@ const Chat = () => {
 
   async function socketInitializer() {
     await fetch("/api/socket");
-
     socket = io();
 
-    socket.on("receive-message", (data) => {
-      setAllMessages((prev) => [...prev, data]);
+    socket.on("connect", () => {
+      if (socket.connected) {
+        socket.on("receive-message", (data) => {
+          setAllMessages((prev) => [...prev, data]);
+        });
+       
+      }
+      setUser(socket.id);
+      setcurrent(socket.id);
     });
   }
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    if(message !="" && username!= ""){
+    if (message != "" && username != "") {
       // console.log("emitted");
       socket.emit("send-message", {
         username,
         message,
+        user,
       });
-      setMessage("");}
-      else{
-        alert('Please enter a valid username and message')
-      }
+      setMessage("");
+    } else {
+      alert("Please enter a valid username and message");
+    }
   }
 
   return (
@@ -50,21 +58,21 @@ const Chat = () => {
             onChange={(e) => setUsername(e.target.value)}
           />
           <div className={styles.container}>
-            {allMessages.map(
-              ({ username, message }, index) =>
-                index % 2 === 0 && (
-                  <div className={styles.text} key={index}>
-                    {username} : {message}
-                  </div>
-                )
-            )}
+            {allMessages.map(({ username, message, user }, index) => (
+              <div
+                className={user === currentuser ? styles.text1 : styles.text}
+                key={index}
+              >
+                {username} : {message}
+              </div>
+            ))}
           </div>
 
           <form className={styles.form} onSubmit={handleSubmit}>
             <input
               className={styles.input}
               name="message"
-              placeholder="enter your message"
+              placeholder=" Enter your message"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               autoComplete={"off"}
